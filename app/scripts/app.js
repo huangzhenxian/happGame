c569fc7569fcc19beca1c7febda11b37394533c0
huangzhenxian <american>
2015-03-29 22:27:58 +0800

    格式化代码而已

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,43 +1,42 @@
 'use strict';
 
-var appModule = angular.module("app",['ngAnimate','ui.router','controllers']);
+var appModule = angular.module("app", ['ngAnimate', 'ui.router', 'controllers']);
 
 appModule.config(['$stateProvider', '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider){
-    $stateProvider
-    .state('main',{
-      url: '/main',
-      templateUrl: 'views/main.html',
-      controller: 'dataLsController'
-    })
-    .state('main.idx',{
-      url: '/idx',
-      templateUrl: 'views/mainIdx.html',
-      //controller: 'mainIdxController'
-    })
-    .state('main.gameLs',{
-      url: '/gameLs',
-      templateUrl: 'views/mainGameLs.html',
-      //controller: 'mainIdxController'
-    })
-    .state('main.gameNews',{
-      url: '/gameNews',
-      templateUrl: 'views/mainGameNews.html',
-      //controller: 'mainIdxController'
-    })
-    .state('main.gameDetail',{
-      url: '/gameDetail/:gameid',
-      templateUrl: 'views/gameDetail.html',
-      controller: 'gameDetailController'
-    });
-    /*state('main.content',{
-      url: '/:content',
-      templateUrl: function($stateParams){
-        console.log($stateParams.content);
-      }
-    })*/
-    
+    function ($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('main', {
+            url: '/main',
+            templateUrl: 'views/main.html',
+            controller: 'dataLsController'
+        })
+            .state('main.idx', {
+                url: '/idx',
+                templateUrl: 'views/mainIdx.html'
+                //controller: 'mainIdxController'
+            })
+            .state('main.gameLs', {
+                url: '/gameLs',
+                templateUrl: 'views/mainGameLs.html'
+                //controller: 'mainIdxController'
+            })
+            .state('main.gameNews', {
+                url: '/gameNews',
+                templateUrl: 'views/mainGameNews.html'
+                //controller: 'mainIdxController'
+            })
+            .state('main.gameDetail', {
+                url: '/gameDetail/:gameid',
+                templateUrl: 'views/gameDetail.html',
+                controller: 'gameDetailController'
+            });
+        /*state('main.content',{
+         url: '/:content',
+         templateUrl: function($stateParams){
+         console.log($stateParams.content);
+         }
+         })*/
 
-    $urlRouterProvider.otherwise('/main/idx');
 
-  }]);
+        $urlRouterProvider.otherwise('/main/idx');
+
+    }]);
